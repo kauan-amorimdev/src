6b00d14df000ed6fe7e68c4fab1a6eda381a21c1
EsauAlves <esau.meet16@gmail.com>
2026-03-21 14:26:51 -0300

    Update main.c
    subprograma para preencher matrizes

--- main.c
+++ main.c
@@ -1,5 +1,19 @@
 #include<stdio.h>
 
+// subprograma para preencher matrizes.
+    void preencherMatriz( int *matriz, int lin, int colun, char name ){
+        
+        //no main, deve ser enviado como parâmetro para "name": "A"(..Matriz A) ou "B"(..Matriz B).
+        //estou assumindo que o usuário terá que criar as duas matrizes (A e b) para serem somadas e tivermos a Matriz C.
+       
+       printf("\n --Preenchendo Matriz %c--", name);
+        for(int i = 0; i < lin; i++){
+            for(int j = 0; j < colun; j++){
+                printf("\n%c[%d][%d]: ", name, i + 1, j + 1);
+                scanf("%d", &matriz[i * colun + j]);
+            }
+        } 
+    }
 int main(){
     return 0;
 }
