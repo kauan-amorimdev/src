cb65544c9e372b90bc375abec8f7d22b30d9e735
Dev Kauã <269943517+kauan-amorimdev@users.noreply.github.com>
2026-03-24 15:02:18 -0300

    Update main.c
    build

--- main.c
+++ main.c
@@ -1,59 +1,60 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-// subprograma para preencher matrizes.
-    void preencherMatriz( int *matriz, int lin, int colun, char name ){
-        
-        //no main, deve ser enviado como parâmetro para "name": "A"(..Matriz A) ou "B"(..Matriz B).
-        //estou assumindo que o usuário terá que criar as duas matrizes (A e b) para serem somadas e tivermos a Matriz C.
-       
-       printf("\n --Preenchendo Matriz %c--", name);
-        for(int i = 0; i < lin; i++){
-            for(int j = 0; j < colun; j++){
-                printf("\n%c[%d][%d]: ", name, i + 1, j + 1);
-                scanf("%d", &matriz[i * colun + j]);
-            }
-        } 
-    }
-// subprograma para somar matrizes.
-    void somarMatrizes(int *A, int *B, int *C, int tam){
-        for(int i = 0; i < tam; i++){
-            C[i] = A[i] + B[i]; 
+void exibirmatriz(int linhas, int colunas, int vetor[linhas * colunas]) {
+    printf("\nMatriz Atual\n");
+    for (int i = 0; i < linhas; i++) {
+        for (int j = 0; j < colunas; j++) {
+            printf("%d ", vetor[i * colunas + j]);
         }
+        printf("\n");
     }
+}
+
+int buscar(int li, int co, int colunas, int linhas, int vetor[linhas * colunas], int elemento) {
+    vetor[(li * colunas) + co] = elemento;
+    return vetor[(li * colunas) + co];
+}
+
+void zerarmatriz(int linhas, int colunas, int vetor[linhas * colunas]) {
+    for (int i = 0; i < linhas * colunas; i++) {
+        vetor[i] = 0;
+    }
+    exibirmatriz(linhas, colunas, vetor);
+}
+
+
+void adicionarelemento(int linhas, int colunas, int vetor[linhas * colunas]) {
+    int elemento, linha, coluna, n = 1;
+    while(n){
+    printf("\nInforme qual elemento deseja adicionar: ");
+    scanf("%d", &elemento);
+    printf("Informe qual a linha e a coluna (ex: 1 2): ");
+    scanf("%d %d", &linha, &coluna);
+
+    linha--; 
+    coluna--;
+
+    if (linha >= 0 && linha < linhas && coluna >= 0 && coluna < colunas) {
+        vetor[linha * colunas + coluna] = buscar(linha, coluna, colunas, linhas, vetor, elemento);
+    } else {
+        printf("\nPosicao invalida!\n");
+    }
+
+    exibirmatriz(linhas, colunas, vetor);
+    printf("Deseja adicionar mais algum elemento? 1 pra sim 0 pra nao");
+    scanf("%d",&n);
+}
+}
+
+int main() {
+    int linhas, colunas; 
+    system("cls");
+    printf("Informe a quantidade de linhas e colunas: ");
+    scanf("%d %d", &linhas, &colunas);
+    int vetor[linhas * colunas];
+    zerarmatriz(linhas, colunas, vetor);
+    adicionarelemento(linhas, colunas, vetor);
     
-// subprograma para exibir matrizes. 
-     void imprimirMatriz( int *mat, int lin, int colun, char name ){
-        
-        printf("---Exibindo Matriz %c---\n", name);
-        for(int i = 0; i < lin; i++){
-            for(int j = 0; j < colun; j++){
-                printf("%d ", mat[i * colun + j]);
-            }
-            printf("\n");
-        } 
-        
-    }
-int main(){
-    int l, c;
-            
-            printf("\nInforme a quantidade de linhas: ");
-            scanf("%d", &l);
-            printf("\nInforme a quantidade de colunas: ");
-            scanf("%d", &c);
-            
-            int tamTotal = l * c;
-            
-            int matA[tamTotal];
-            int matB[tamTotal];
-            int matC[tamTotal];
-            
-            preencherMatriz(matA, l, c, 'A');
-            imprimirMatriz(matA, l, c, 'A');
-           
-            preencherMatriz(matB, l, c, 'B');
-            imprimirMatriz(matB, l, c, 'B');
-           
-            somarMatrizes(matA, matB, matC, tamTotal);
-            imprimirMatriz(matC, l, c, 'C');
     return 0;
 }
