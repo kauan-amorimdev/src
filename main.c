6c9e555612e788c4380cb1609e87bf33066698ef
Dev Kauã <269943517+kauan-amorimdev@users.noreply.github.com>
2026-03-24 18:51:02 -0300

    Update main.c

--- main.c
+++ main.c
@@ -10,11 +10,13 @@ void exibirmatriz(int linhas, int colunas, int vetor[linhas * colunas]) {
         printf("\n");
     }
 }
-
-int buscar(int li, int co, int colunas, int linhas, int vetor[linhas * colunas], int elemento) {
-    vetor[(li * colunas) + co] = elemento;
-    return vetor[(li * colunas) + co];
-}
+int verificarposicao(int linha, int coluna, int colunas, int linhas){
+    if (linha >= 0 && linha < linhas && coluna >= 0 && coluna < colunas) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
 
 void zerarmatriz(int linhas, int colunas, int vetor[linhas * colunas]) {
     for (int i = 0; i < linhas * colunas; i++) {
@@ -24,26 +26,40 @@ void zerarmatriz(int linhas, int colunas, int vetor[linhas * colunas]) {
 }
 
 
-void adicionarelemento(int linhas, int colunas, int vetor[linhas * colunas]) {
-    int elemento, linha, coluna, n = 1;
-    while(n){
-    printf("\nInforme qual elemento deseja adicionar: ");
-    scanf("%d", &elemento);
-    printf("Informe qual a linha e a coluna (ex: 1 2): ");
-    scanf("%d %d", &linha, &coluna);
-
-    linha--; 
-    coluna--;
-
-    if (linha >= 0 && linha < linhas && coluna >= 0 && coluna < colunas) {
-        vetor[linha * colunas + coluna] = buscar(linha, coluna, colunas, linhas, vetor, elemento);
-    } else {
-        printf("\nPosicao invalida!\n");
+void inserir(int li, int co, int colunas, int linhas, int vetor[linhas * colunas], int elemento) {
+    if (vetor[(li * colunas) + co] != 0)
+    {
+        int confirm;
+        printf("Item preenchido. deseja substituir? 1 para sim 0 para nao ");
+        scanf("%d", &confirm);
+        if (confirm != 1)
+        {
+            return;
+        }
     }
-
+    vetor[(li * colunas) + co] = elemento;
     exibirmatriz(linhas, colunas, vetor);
-    printf("Deseja adicionar mais algum elemento? 1 pra sim 0 pra nao");
-    scanf("%d",&n);
+}
+    
+    void adicionarelemento(int linhas, int colunas, int vetor[linhas * colunas]) {
+        int elemento, linha, coluna, n = 1;
+        while(n){
+            printf("\nInforme qual elemento deseja adicionar: ");
+            scanf("%d", &elemento);
+            printf("Informe qual a linha e a coluna (ex: 1 2): ");
+            scanf("%d %d", &linha, &coluna);
+            linha--; 
+            coluna--;
+            if (verificarposicao(linha, coluna, colunas, linhas))
+            {
+                inserir(linha, coluna, colunas, linhas, vetor, elemento);
+            }
+            else{
+                printf("\nPosicao invalida!\n");
+
+            }
+        printf("Deseja adicionar mais algum elemento? 1 pra sim 0 pra nao: ");
+        scanf("%d",&n);
 }
 }
 
